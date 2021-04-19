6193bb5691d09e75ca7cded10cb80f3a470f5d6b
Mickael VIDAL <mickael.vidal51@gmail.com>
2021-04-19 15:23:59 +0200

    Ajout de la fonction permettant de mélanger les lettres du mot random de la fonction tirageDuMot()

--- maximo_1.java
+++ maximo_1.java
@@ -3,6 +3,9 @@ package fr.eni.tpmod1;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class maximo_1 {
 
@@ -11,24 +14,31 @@ public class maximo_1 {
 		 *	L'objectif est de créer un jeu de lettres : le joueur voit un tirage de lettres et doit trouver un
 			mot le plus long possible en utilisant ces lettres. Voici comment se déroule l algorithme du
 			programme :
-			- Un mot est tiré au sort dans le dictionnaire
-			- Les lettres le constituant sont mélangées aléatoirement
-			- Le tirage est affiché au joueur
-			- Le joueur saisi t sa proposition
-			- La proposition est vérifiée :
+			|X| Un mot est tiré au sort dans le dictionnaire
+			|X| Les lettres le constituant sont mélangées aléatoirement
+			|O| Le tirage est affiché au joueur
+			|O| Le joueur saisit sa proposition
+			|O| La proposition est vérifiée :
 			--> uniquement les lettres tirées ont été utilisées
 			--> mot présent dans le dictionnaire
-			- Le mot tiré au sort est dévoilé au joueur.*/
-		System.out.println(tirageDuMot());
-	}
+			|O| Le mot tiré au sort est dévoilé au joueur.*/
+		
+		String mot = tirageDuMot();
+		System.out.println(mot);
+		System.out.println(motMelange(mot));
+
+	};
 	
 
-	@SuppressWarnings("unused")
+	
 	/**
-	 * Fonction qui va retourner le mot tiré au sort.
-	 * Récupération de la liste de mots dans un tableau.*/
+	 * Fonction qui retourne le mot tiré au sort.
+	 * Récupération de la liste de mots dans un tableau.
+	 * Puis récupération d'un mot random du tableau.
+	 * @return
+	 */
 	private static String tirageDuMot() {
-		String mot = "";
+		String motRandom = "";
 		String[] dicoTab = new String[22506];
 		int randomTab = (int) (Math.random() * 22506 + 1);
 		
@@ -42,9 +52,27 @@ public class maximo_1 {
 			System.out.println("Raté");
 		}
 		
-		mot = dicoTab[randomTab];
+		motRandom = dicoTab[randomTab];
 		
-		return mot;
+		return motRandom;
+	}
+	
+
+	/**
+	 * Fonction qui récupère le mot tiré en random du dico
+	 * pour en mélanger les lettres.
+	 * @param mot
+	 * @return
+	 */
+	public static String motMelange(String mot) {
+		List<String> lettres = Arrays.asList(mot.split(""));
+		Collections.shuffle(lettres);
+		String test = "";
+		
+		for (String lettre : lettres) {
+			test += lettre;
+		}
+		return test;
 	}
 	
 }
