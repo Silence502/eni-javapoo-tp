0a9eaee3a70d47db8c8ad43d57f97be675880747
Mickael VIDAL <mickael.vidal51@gmail.com>
2021-04-20 18:27:42 +0200

    Update : Amélioration de l'affichage et ajout d'un menu et de possiblité de rejouer (voir update_maximo_1.txt)

--- maximo_1.java
+++ maximo_1.java
@@ -3,13 +3,13 @@ package fr.eni.tpmod1;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
+
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 public class maximo_1 {
-
+	static final Scanner s = new Scanner(System.in);
 	public static void main(String[] args) {
 		/*
 		 *	L'objectif est de créer un jeu de lettres : le joueur voit un tirage de lettres et doit trouver un
@@ -21,10 +21,13 @@ public class maximo_1 {
 			|X| Le joueur saisit sa proposition
 			|X| La proposition est vérifiée :
 			--> uniquement les lettres tirées ont été utilisées
-			--> mot présent dans le dictionnaire
 			|X| Le mot tiré au sort est dévoilé au joueur.*/
 		
+		menu();
 		jeu();
+		
+		
+		
 	}
 	
 	/**
@@ -42,7 +45,7 @@ public class maximo_1 {
 				Scanner s = new Scanner(dico)) {
 			while (s.hasNextLine())
 				for (int i = 0; i < dicoTab.length; i++) {
-					dicoTab[i] = s.nextLine();
+					dicoTab[i] = s.nextLine().toLowerCase();
 				}
 		} catch (IOException e) {
 			System.out.println("Raté");
@@ -85,53 +88,119 @@ public class maximo_1 {
 	private static void controleSaisie(String tirage, String saisie) {
 		char lettre1 = 0;
 		char lettre2 = 0;
-		boolean test;
+		boolean test = false;
 		
-		Scanner s = new Scanner(System.in);
+		
+		
 		
 		do {
-			test = true;
-			System.out.print("Votre proposition : ");
-			saisie = s.nextLine();
 			
-			while (saisie.length() != tirage.length()) {
-				System.out.printf("Le nombre de lettres ne correspond pas au tirage.%n"
-						+ "Réessayez : ");
-				saisie = s.nextLine();
+			while (test == false) {
+				String ml = "";
+				test = true;
+				System.out.println("----------------------------------------\n"
+						+ "Votre proposition : ");
+				saisie = s.nextLine().toLowerCase();
+				
+				while (saisie.length() != tirage.length()) {
+					System.out.printf("Le nombre de lettres ne correspond pas au tirage (%d).%n"
+							+ "Réessayez : ", tirage.length());
+					saisie = s.nextLine();
+				}
+				int i = 0;
+				//List<String> mc = Arrays.asList(null)
+				while (i < saisie.length()) {
+					int j = 0;
+					lettre1 = saisie.charAt(i);
+					i++;
+					while (lettre1 != lettre2 && j != tirage.length()) {
+						lettre2 = tirage.charAt(j);
+						j++;
+						
+						if (lettre1 != lettre2 && j == tirage.length()) {
+							//System.out.println(lettre1 + " n'est pas dans le mot.");
+							ml += lettre1;
+							test = false;
+						}
+					}
+				}
+				if (ml != "") {
+					System.out.printf("La/les lettre(s) '%s' ne sont pas dans le mot.%n", ml);
+				}
 			}
 			
-			int i = 0;
-			while (i < saisie.length()) {
-				int j = 0;
-				lettre1 = saisie.charAt(i);
-				i++;
-				while (lettre1 != lettre2 && j != tirage.length()) {
-					lettre2 = tirage.charAt(j);
-					j++;
-					if (lettre1 != lettre2 && j == tirage.length()) {
-						System.out.println(lettre1 + " n'est pas dans le mot.");
-						test = false;
-					}
+			if (tirage.equals(saisie)) {
+				System.out.printf("Bravo : '%s' est une bonne réponse !%n"
+						+ "----------------------------------------%n", saisie);
+			} else {
+				System.out.printf("'%s' est une mauvaise réponse.%n", saisie);
+				System.out.print("Voulez-vous réessayer ? ('o' = oui | 'n' = non) : ");
+				saisie = s.nextLine().toLowerCase();
+				switch (saisie) {
+				case "o":
+					test = false;
+					break;
+				case "n":
+					System.out.printf("La réponse était : %s%n"
+							+ "----------------------------------------%n", tirage);
+					test = true;
+					break;
+				default:
+					System.out.println("Saisie incorrecte.");
+					break;
 				}
 			}
 		} while (test == false);
 		
-		if (tirage.equals(saisie)) {
-			System.out.printf("%s est une bonne réponse !", saisie);
-		} else {
-			System.out.printf("%s est une mauvaise réponse. Le mot était : %s", saisie, tirage);
-		}
-		s.close();
 	}
 	
 	/**
 	 * Corps du jeu avec saisie et appel des fonctions et procédures.
 	 */
-	private static void jeu() {
+	private static void tirage() {
 		String tirage = tirageDuMot();
 		String motMelange = motMelange(tirage);
 		String saisie = "";
 		affichageTirage(motMelange);
 		controleSaisie(tirage, saisie);
+	}
+	
+	/**
+	 * Affichage du menu du jeu.
+	 */
+	private static void menu() {
+		//Scanner s =new Scanner(System.in);
+		System.out.println("----------------------------------------\n"
+						 + "----------------Bienvenu----------------\n"
+						 + "----------------------------------------");
+		System.out.println("Appuyez sur une touche pour commencer");
+		s.nextLine();
+	}
+	
+	/**
+	 * Lancement du programme avec possibilité de rejouer.
+	 */
+	private static void jeu() {
+		boolean recommencer = true;
+		
+		do {
+			tirage();
+			System.out.println("Voulez-vous recommencer avec un nouveau mot ? ('o' = oui | 'n' = non) : ");
+			String choix = s.nextLine().toLowerCase();
+			switch (choix) {
+			case "o":
+				recommencer = false;
+				break;
+			case "n":
+				System.out.println("A bientôt !");
+				recommencer = true;
+				break;
+			default:
+				recommencer = true;
+				break;
+			}
+		} while (recommencer == false);
+		
+		s.close();
 	}
 }
